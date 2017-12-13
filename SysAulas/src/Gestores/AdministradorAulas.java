2b2d5e09a79e889064d1acc1aa5012b6ee50b1e7
Unknown <lucianomar96@gmail.com>
2017-12-13 12:00:34 -0300

    Detalles

--- SysAulas/src/Gestores/AdministradorAulas.java
+++ SysAulas/src/Gestores/AdministradorAulas.java
@@ -15,6 +15,7 @@ import db.model.InfoAulasDisponibles;
 import db.model.Informatica;
 import db.model.Multimedios;
 import db.model.Periodica;
+import db.model.Reserva;
 import db.model.SinRecursos;
 import java.sql.Time;
 import java.util.ArrayList;
@@ -37,7 +38,38 @@ public class AdministradorAulas {
             Dia d = (Dia) o;
             fechas.addAll(d.getFechas());
             
-            for (Fecha f : fechas) {
+            opciones = obtenerInfoAulasDisponibles(nuevaReserva, tipoAula, opciones, fechas);
+            
+            fechas = new ArrayList<>();
+        }
+        
+        return opciones;
+    }
+
+    public static ArrayList<InfoAulasDisponibles> obtenerAulas(Esporadica nuevaReserva, String tipoAula) {
+        
+        ArrayList<InfoAulasDisponibles> opciones = new ArrayList<>();
+        
+        ArrayList<Fecha> fechas = new ArrayList<>();
+        fechas.addAll(nuevaReserva.getFechas());
+
+        opciones = obtenerInfoAulasDisponibles(nuevaReserva, tipoAula, opciones, fechas);
+        
+        return opciones;
+    }
+    
+    public static Multimedios deproxyMultimedios(Aula a) {
+        return AulaDao.unProxyToClass(a, Multimedios.class);
+    }
+    public static Informatica deproxyInformatica(Aula a) {
+        return AulaDao.unProxyToClass(a, Informatica.class);
+    }
+    public static SinRecursos deproxySinRecursos(Aula a) {
+        return AulaDao.unProxyToClass(a, SinRecursos.class);
+    }
+    
+    private static ArrayList<InfoAulasDisponibles> obtenerInfoAulasDisponibles(Reserva nuevaReserva, String tipoAula, ArrayList<InfoAulasDisponibles> opciones, ArrayList<Fecha> fechas) {
+        for (Fecha f : fechas) {
                 HashSet<Integer> rechazadas = new HashSet<>();
                 //System.out.println("-------------------------------------------------------------------------");
                 //System.out.println((java.sql.Date) f.getFecha());
@@ -56,8 +88,8 @@ public class AdministradorAulas {
                 ArrayList<Aula> aulas = AulaDao.findComplemento(rechazadasAux);
                 //System.out.println(aulas);
                 
-                ArrayList<Aula> quitar = new ArrayList<>();
                 if (!aulas.isEmpty()) {
+                    ArrayList<Aula> quitar = new ArrayList<>();
                     for(Aula a : aulas) {
                         //System.out.print(a.getIdAula());
                         if (a.getCapacidad() < nuevaReserva.getCantidadAlumnos()) {
@@ -109,101 +141,6 @@ public class AdministradorAulas {
                     opciones.add(iAD);
                 }
             }
-            fechas = new ArrayList<>();
-        }
-        
         return opciones;
     }
-
-    public static ArrayList<InfoAulasDisponibles> obtenerAulas(Esporadica nuevaReserva, String tipoAula) {
-        
-        ArrayList<InfoAulasDisponibles> opciones = new ArrayList<>();
-        
-        ArrayList<Fecha> fechas = new ArrayList<>();
-        fechas.addAll(nuevaReserva.getFechas());
-
-        for (Fecha f : fechas) {
-            HashSet<Integer> rechazadas = new HashSet<>();
-            //System.out.println("-------------------------------------------------------------------------");
-            //System.out.println((java.sql.Date) f.getFecha());
-            //System.out.println("-------------------------------------------------------------------------");
-            
-            ArrayList<Fecha> fechasRechazadas = FechaDao.findBetween((java.sql.Date) f.getFecha(), (Time) f.getHoraInicio(), f.getDuracion());
-            for(Fecha fec : fechasRechazadas) {
-                rechazadas.add(fec.getAula().getIdAula());
-            }
-                
-            //System.out.println(rechazadas);
-
-            ArrayList<Integer> rechazadasAux = new ArrayList<>();
-            rechazadasAux.addAll(rechazadas);
-
-            ArrayList<Aula> aulas = AulaDao.findComplemento(rechazadasAux);
-            //System.out.println(aulas);
-            if (!aulas.isEmpty()) {
-                ArrayList<Aula> quitar = new ArrayList<>();
-                for(Aula a : aulas) {
-                    //System.out.print(a.getIdAula());
-                    if (a.getCapacidad() < nuevaReserva.getCantidadAlumnos()) {
-                        quitar.add(a);
-                        //System.out.println(" borrado(1)");
-                    }
-                    switch(tipoAula) {
-                        case "multi":
-                            if (!(Hibernate.getClass(a) == Multimedios.class)) {
-                                quitar.add(a);//System.out.println(" borrado(2)");
-                            }
-                            break;
-                        case "info":
-                            if (!(Hibernate.getClass(a) == Informatica.class)) {
-                                quitar.add(a);//System.out.println(" borrado(3)");
-                            }
-                            break;
-                        case "sinrec":
-                            if (!(Hibernate.getClass(a) == SinRecursos.class)) {
-                                quitar.add(a);//System.out.println(" borrado(4)");
-                            }
-                            break;
-                    }
-                }
-                for(Aula a : quitar) {
-                    aulas.remove(a);
-                }
-
-                if (aulas.size() >= 2) {
-                    Collections.sort(aulas, new Comparator<Aula>() {
-                        public int compare(Aula a1, Aula a2){
-                           return a1.getCapacidad() - a2.getCapacidad();
-                        }
-                    });
-                }
-                //System.out.println(rechazadas);
-                ArrayList<Aula> ultimasTres = new ArrayList<>();
-                if (aulas.size() >= 1) { ultimasTres.add(aulas.get(0)); };
-                if (aulas.size() >= 2) { ultimasTres.add(aulas.get(1)); };
-                if (aulas.size() >= 3) { ultimasTres.add(aulas.get(2)); };
-                InfoAulasDisponibles iAD = new InfoAulasDisponibles(f);
-                iAD.setOpcionesAulas(ultimasTres);
-                opciones.add(iAD);
-            } else {
-                ArrayList<Aula> ultimasTres = new ArrayList<>();
-                InfoAulasDisponibles iAD = new InfoAulasDisponibles(f);
-                iAD.setOpcionesAulas(ultimasTres);
-                opciones.add(iAD);
-            }
-        }
-        return opciones;
-    }
-    
-    public static Multimedios deproxyMultimedios(Aula a) {
-        return AulaDao.unProxyToClass(a, Multimedios.class);
-    }
-    public static Informatica deproxyInformatica(Aula a) {
-        return AulaDao.unProxyToClass(a, Informatica.class);
-    }
-    public static SinRecursos deproxySinRecursos(Aula a) {
-        return AulaDao.unProxyToClass(a, SinRecursos.class);
-    }
-    
-    
 }
